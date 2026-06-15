1a2e046bf2ea61e58c8bf335a9c2230a320b8060
Alee <Earnwithalee7890@users.noreply.github.com>
2026-06-16 02:52:13 +0500

    feat: import wagmi connection hooks in WalletConnect.js

--- src/components/WalletConnect.js
+++ src/components/WalletConnect.js
@@ -1,5 +1,7 @@
 "use client";
 import { usePrivy } from "@privy-io/react-auth";
+import { useConnect, useAccount } from "wagmi";
+import { injected } from "wagmi/connectors";
 import { useState, useEffect } from "react";
 
 export default function WalletConnect() {
